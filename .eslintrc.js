05fbb49670e2b7c9b104201c68ed4e3dad7d45ca
eowiz <eowiz@proton.me>
2022-08-03 23:02:11 +0900

    Update rule in .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,23 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'standard',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
+    "standard",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    quotes: ["error", "double"],
+  },
 };
