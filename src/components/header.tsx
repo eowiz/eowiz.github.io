3277f65ba7d94a3cd8089017a1d6a26099db1e48
eowiz <eowiz@proton.me>
2022-09-03 02:34:12 +0900

    Remove storybook

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
-import { useStaticQuery, graphql, PageProps, Link } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
+import Link from "gatsby-link";
 import * as React from "react";
 require("./header.css");
 
